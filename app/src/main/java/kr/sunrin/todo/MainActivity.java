ab8e06d0bbd2ecd09e181d31262dfb800593ea04
Zeroble <dlalsdyd0219@gmail.com>
2017-06-26 17:35:00 +0900

    asdf

--- app/src/main/java/kr/sunrin/todo/MainActivity.java
+++ app/src/main/java/kr/sunrin/todo/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -24,16 +26,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.main_toorbar);
-        setSupportActionBar(toolbar); //v7 only
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowTitleEnabled(true);
-            getSupportActionBar().setTitle(getString(R.string.app_name));
-        }
-
         container = (FrameLayout) findViewById(R.id.container);
         noTodoContainer = (LinearLayout) findViewById(R.id.no_to_container);
+        toolbar = (Toolbar) findViewById(R.id.main_toorbar);
+        listView = (ListView) findViewById(R.id.main_list);
+        setSupportActionBar(toolbar); //v7 only
+
+        if (getSupportActionBar() != null) { //안정성을 위해 null체크
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            getSupportActionBar().setTitle(R.string.app_name);
+        }
+
         findViewById(R.id.main_fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,5 +46,25 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.main_list);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.setting:
+                Toast.makeText(this,"Setting",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.about :
+                Toast.makeText(this,"Aobut",Toast.LENGTH_LONG).show();
+                break;
+
+        }
+        return true;
     }
 }
